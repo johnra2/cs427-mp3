e4a35a1948469c414cee3bc68036448dd4458f0a
John Armgardt <johnra2@illinois.edu>
2021-10-18 23:55:03 -0500

    Sort tests 2

--- src/test/java/BlackBox/SortAlgorithmBlackBoxTest.java
+++ src/test/java/BlackBox/SortAlgorithmBlackBoxTest.java
@@ -10,10 +10,13 @@ public class SortAlgorithmBlackBoxTest extends SortSetup {
         int[][][] options = new int[][][] {
             {{2, 1}, {1, 2}},
             {{0, 1, -1}, {-1, 0, 1}},
-            {{Integer.MAX_VALUE, Integer.MIN_VALUE}, {Integer.MIN_VALUE, Integer.MAX_VALUE}},
+            {{Integer.MAX_VALUE, 0}, {0, Integer.MAX_VALUE}},
+            {{0, Integer.MIN_VALUE,}, {Integer.MIN_VALUE, 0}},
             {{10, 2, 6, 1, 5, 9, 5, 3, 5, 8, 4, 7}, {1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10}},
+            {{1,1,1,1,1,1,1,1,1,0,0,0}, {0,0,0,1,1,1,1,1,1,1,1,1}},
             {null, null},
             {{}, {}},
+            {{0}, {0}},
         };
         for (int[][] option : options){
             int[] input = option[0];
@@ -24,7 +27,7 @@ public class SortAlgorithmBlackBoxTest extends SortSetup {
 
     @Test
     public void longTest() {
-        int length = 100000;
+        int length = 1000000;
         int[] input = new int[length];
         int[] expectedOutput = new int[length];
         for (int i = 0; i < length; i++) {
